a140f3f7fbcca5ea4d25837be8c64440146646eb
Kawaharasouta <kawahara6514@gmail.com>
2018-05-13 01:46:53 +0900

    Cut out debugging functions

--- dpdk.c
+++ dpdk.c
@@ -38,9 +38,6 @@
 #include"dpdk.h"
 #include"microps.h"
 
-//#include<net/ethernet.h>
-//#include<netinet/if_ether.h>
-
 
 struct device {
 	int fd;
@@ -51,44 +48,6 @@ struct rte_mempool *mbuf_pool;
 static const struct rte_eth_conf port_conf_default = {
 	.rxmode = { .max_rx_pkt_len = ETHER_MAX_LEN }
 };
-
-
-void hexdump(u_int16_t *buf, int size){
-  int i;
-  for (i = 0;i < size; i++){
-    fprintf(stdout, "%04x ", *(buf + i));
-    if ((i + 1) % 8 == 0){ 
-      fprintf(stdout, "\n");
-    }   
-  }
-  fprintf(stdout, "\nfin\n");
-}
-
-void print_mbuf(const struct rte_mbuf *bufs){
-	printf("-----print_mbuf-----\n");
-
-	//printf("rearm_data: %u\n", bufs->rearm_data);
-
-	printf("data_off: %u\n", bufs->data_off);
-	printf("refcnt: %u\n", bufs->refcnt);
-	printf("nb_segs: %u\n", bufs->nb_segs);
-	printf("port: %u\n", bufs->port);
-	printf("ol_flags: %u\n", bufs->ol_flags);
-	printf("packet_type: %u\n", bufs->packet_type);
-	printf("pkt_len: %u\n", bufs->pkt_len);
-	printf("data_len: %u\n", bufs->data_len);
-	printf("vlan_tci: %u\n", bufs->vlan_tci);
-	printf("rss: %u\n", bufs->hash.rss);
-	printf("vlan_tci_outer: %u\n", bufs->vlan_tci_outer);
-	printf("buf_len: %u\n", bufs->buf_len);
-	printf("timestamp: %u\n", bufs->timestamp);
-	printf("udata: %u\n", bufs->udata64);
-	printf("tx_offlead: %u\n", bufs->tx_offload);
-	printf("priv_size: %u\n", bufs->priv_size);
-	printf("timesync: %u\n", bufs->timesync);
-	printf("seqn: %u\n", bufs->seqn);
-	printf("--------------------\n");
-}
 
 
 /*
@@ -156,7 +115,6 @@ port_init(uint16_t port/*, struct rte_mempool *mbuf_pool*/)
 
 
 void dpdk_init(int argc, char **argv){
-	//struct rte_mempool *mbuf_pool;
 	int ret;
 	unsigned nb_ports;
 	uint16_t portid;
@@ -191,14 +149,8 @@ void dpdk_init(int argc, char **argv){
 
 
 
-
-
-
-
-
-
+/* Wrapped a function to control device but not practically meaningful */
 device_t *device_open (const char *name) {
-	printf("head of device_open\n");
 	device_t *device;
 	if ((device = malloc(sizeof(*device))) == NULL) {
 		perror("malloc");
@@ -206,43 +158,18 @@ device_t *device_open (const char *name) {
 	}
 	return device;
 }
-
 void device_close (device_t *device) {
-    //if (device->fd != -1) {
-    //    close(device->fd);
-    //}
     free(device);
 }
 
 
 
 
-//target api of input
+//!target api of input
 void
 device_input (device_t *device, void (*callback)(uint8_t *, size_t), int timeout) {
-    //struct pollfd pfd;
-    //ssize_t ret, length;
-    //uint8_t buffer[2048];
-
-    //pfd.fd = device->fd;
-    //pfd.events = POLLIN;
-    //ret = poll(&pfd, 1, timeout);
-    //if (ret <= 0) {
-    //    return;
-    //}   
-    //length = read(device->fd, buffer, sizeof(buffer));
-    //if (length <= 0) {
-    //    return;
-    //}   
-    //callback(buffer, length);
-
- //return write(device->fd, buffer, length);
-
- //printf("head of device_input\n");
-
 	const uint16_t nb_ports = rte_eth_dev_count();
 	uint16_t port;
-
 
   /*
    * Check that the port is on the same NUMA node as the polling thread
@@ -256,17 +183,9 @@ device_input (device_t *device, void (*callback)(uint8_t *, size_t), int timeout
           "polling thread.\n\tPerformance will "
           "not be optimal.\n", port);
   
- // printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",
- //     rte_lcore_id());
 
-	
-
-	port = 0;
 	/* Recv burst of RX packets */
-	//struct rte_mbuf *bufs[BURST_SIZE];
-
-	//printf("before rx_burst\n");
-
+	port = 0;
 	struct rte_mbuf *bufs[BURST_SIZE];
 	const uint16_t nb_rx = rte_eth_rx_burst(port, 0, bufs, BURST_SIZE);
 
@@ -274,39 +193,18 @@ device_input (device_t *device, void (*callback)(uint8_t *, size_t), int timeout
 	for (i = 0; i < nb_rx ; i++){
 		uint8_t *p = rte_pktmbuf_mtod(bufs[i], uint8_t*);
 		size_t size = rte_pktmbuf_pkt_len(bufs[i]);
-	
-	//FILE *fp;
-	//fp = fopen("txit.txt", "a");
-	hexdump(p, size);
-	print_mbuf(bufs[i]);
 
-	//fprintf(fp, "%s\n", hexdump(bufs, nb_rx));
-	//close(fp);
-
-	callback(p, size);
+		callback(p, size);
 	}
-	//return rte_eth_tx_burst(port, 0, buffer, 1);
-
 }
 
 
-
-
-
-
-
-
-
-//target api of output
+//!target api of output
 ssize_t
 device_output (device_t *device, const uint8_t *buffer, size_t length) {
- //return write(device->fd, buffer, length);
-
-	printf("head of device_output\n");
 	struct rte_mbuf *bufs[BURST_SIZE];
 	const uint16_t nb_ports = rte_eth_dev_count();
 	uint16_t port;
-
 
   /*
    * Check that the port is on the same NUMA node as the polling thread
@@ -320,64 +218,22 @@ device_output (device_t *device, const uint8_t *buffer, size_t length) {
           "polling thread.\n\tPerformance will "
           "not be optimal.\n", port);
   
-  printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",
-      rte_lcore_id());
-
-	port = 0;
 	/* Send burst of TX packets */
-	//struct rte_mbuf *bufs[BURST_SIZE];
+	port = 0;
 	
-	printf("rte_pktmbuf_alloc\n");
 	bufs[0] = rte_pktmbuf_alloc(mbuf_pool);
-	print_mbuf(bufs[0]);
 	bufs[0]->pkt_len = length;
 	bufs[0]->data_len = length;
-	//print_mbuf(bufs[0]);
 
-	printf("strncpy\n");
-	//uint8_t *p = rte_pktmbuf_append(bufs[0], length);
 	uint8_t *p = rte_pktmbuf_mtod(bufs[0], uint8_t*);
 	memcpy(p, buffer, length);
-	
-	/******/
-	//struct ether_arp *pp = p + sizeof(struct ether_header);
-	//struct ether_arp *buffer_e += sizeof(struct ether_header);
-	//strncpy(pp->ea_hdr, buffer_e->ea_hdr, strlen(buffer->ea_hdr));
-	/******/
 
-	print_mbuf(bufs[0]);
-	printf("port\n");
 	bufs[0]->port = 0;
-	printf("packet_type\n");
 	bufs[0]->packet_type = 1;
-	print_mbuf(bufs[0]);
-	printf("len\n");
-	//printf("rte_pktmbuf_read\n");
-	//bufs[0] = rte_pktmbuf_read(bufs[0], 0, length, buffer);
-	////print_mbuf(bufs[0]);
-
-	//printf("rte_get_ptypte_name\n");
-	////bufs[0]->packet_type = (uint32_t)193;
-	//if (rte_get_ptype_name(bufs[0]->packet_type, buffer, length) == -1){
-	//	printf("rte_get_ptype_name\n");
-	//	exit(1);
-	//}
-
-	//print_mbuf(bufs[0]);
-	
-	//printf("mbuf test\n");
-	//uint8_t *p = rte_pktmbuf_mtod(bufs[0], uint8_t*);
-	//hexdump(buffer, length);
-	printf("*****\n");
-	hexdump(p, length);
-
-	printf("before tx_burst\n");
 
 	if (rte_eth_tx_burst(port, 0, bufs, 1) > 0){
 		return length;
 	}
 	return -1;
 }
-
-
 
